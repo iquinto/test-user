36a163ab16a3cbae3663f0125d5c92fdd6cf0163
Isagani JR Quinto Gomez <iquinto@dipta.cat>
2022-12-09 11:23:11 +0100

    add filter
    add filter

--- src/main/java/edu/uoc/pds/user/application/rest/UserRESTController.java
+++ src/main/java/edu/uoc/pds/user/application/rest/UserRESTController.java
@@ -14,6 +14,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.servlet.http.HttpServletRequest;
 import java.net.URI;
 import java.util.List;
 
@@ -86,6 +87,15 @@ public class UserRESTController {
                 .toUri();
 
         return ResponseEntity.created(uri).body(user);
+    }
+
+
+    @GetMapping("/test/something")
+    public ResponseEntity<?> getOneBook( HttpServletRequest request) {
+
+        System.out.println("HIAALLALALALLALA " );
+
+        return ResponseEntity.ok("OK");
     }
 
 
